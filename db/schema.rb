7b738d39a86f245d817058295e96899889a67fbd
Admir23 <hello@admir-kurtovic.info>
2017-08-09 22:50:47 +0200

    Added Playlist and Refrence to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806123551) do
+ActiveRecord::Schema.define(version: 20170809204130) do
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_playlists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
