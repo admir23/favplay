be68452ae72ca1862b5b7fce7a1be2ba50053e7b
Admir23 <hello@admirkurtovic.info>
2017-09-17 19:31:38 +0200

    working on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 	def index
+		@songs = current_user.songs.paginate(:page => params[:page], :per_page => 5).order(created_at: :desc)
 		@latest_albums = Album.order('created_at DESC').last(5)
-		@top_songs = Favorite.joins("LEFT OUTER JOIN songs ON favorites.song_id = songs.id").select("favorites.*,songs.name as name, songs.artist_id as artist_id").group(:song_id).order('COUNT(songs.id) DESC')
+		@top_songs = Favorite.joins("LEFT OUTER JOIN songs ON favorites.song_id = songs.id").select("favorites.id,songs.name as name, songs.artist_id as artist_id").group(:song_id).order('COUNT(songs.id) DESC')
   .limit(10)
 	end
 end		
