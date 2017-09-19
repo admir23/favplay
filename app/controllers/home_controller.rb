8dbf8b17770eb4759db1710a851a2d9509ea1d94
Admir23 <hello@admirkurtovic.info>
2017-09-19 18:45:32 +0200

    adding more stlyes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
 	def index
 		@songs = current_user.songs.paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
 		@latest_albums = Album.order('created_at DESC').last(5)
-		@top_songs = Favorite.joins("LEFT OUTER JOIN songs ON favorites.song_id = songs.id").select("favorites.id,songs.name as name, songs.artist_id as artist_id").group(:song_id).order('COUNT(songs.id) DESC')
-  .limit(10)
+		@top_songs = Favorite.joins("LEFT OUTER JOIN songs ON favorites.song_id = songs.id")
+		                     .select("favorites.id,songs.name as name, songs.artist_id as artist_id")
+		                     .group(:song_id).order('COUNT(songs.id) DESC')
+                         .limit(10)
 	end
 end		
