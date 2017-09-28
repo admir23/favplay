f00f30bbf57323e9127f2f2abe8c3416654a59c3
Admir23 <hello@admirkurtovic.info>
2017-09-28 23:33:40 +0200

    error pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/404', :to => 'errors#not_found'
+  get '/505', :to => 'errors#internal_server_error'
+
   root "home#index"
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
